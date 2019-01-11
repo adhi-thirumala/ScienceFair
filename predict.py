8edb99d85d907fad4d3ccd2da6fc8c47912cf25f
chesnaught7 <adhi.thirumala@gmail.com>
2019-01-10 19:25:27 -0500

    lel it works completely

--- predict.py
+++ predict.py
@@ -7,6 +7,13 @@ import requests
 from PIL import Image
 import numpy as np
 
+import smtplib
+from email.MIME.multipart import MIMEMultipart
+from email.MIME.text import MIMEText
+from email.MIME.base import MIMEBase
+from email import encoders
+
+
 options = {"model": "yolov2-tiny.cfg", "load": "yolov2-tiny.weights", "threshold": 0.2}
 
 tfnet = TFNet(options)
@@ -29,5 +36,34 @@ while True:
             print("bird detected")
             birdsSeen += 1
             curr_img.save('%i.jpg' % birdsSeen)
+            fromaddr = "adhi.thirumala@gmail.com"
+            toaddr = "adhi.thirumala@gmail.com"
+
+            msg = MIMEMultipart()
+
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = "Your camera saw a bird"
+
+            body = "Here is the pic."
+
+            msg.attach(MIMEText(body, 'plain'))
+
+            filename = '%i.jpg' % birdsSeen
+            attachment = open('%i.jpg' % birdsSeen, "rb")
+
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload((attachment).read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+            msg.attach(part)
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(fromaddr, "adhitya2006")
+            text = msg.as_string()
+            server.sendmail(fromaddr, toaddr, text)
+            server.quit()
     print('running again')
     time.sleep(4)
